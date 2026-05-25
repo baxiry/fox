15a4e5ba05eba48c37c9225e9cca85e5b05d86e3
baxiry <ahmedalbachiry@gmail.com>
2026-05-25 15:53:42 +0300

    feat: start suport enums

--- fox/main_gen.c
+++ fox/main_gen.c
@@ -4,18 +4,22 @@
 
 #include "foxgc/fgc.h"
 
-typedef struct {
+typedef struct GNode {
     int32_t id;
-    int32_t padding[256];
-} User;
+    struct GNode* link;
+} GNode;
 
 int main() {
     int32_t stack_top_anchor;
     fgc_init(&stack_top_anchor);
     int32_t i;
-    for (i = 0; (i < 1000000); i = (i + 1)) {
-                    User* u = (User*)((char*)fgc_alloc(6, 1, 0) + 8);
-        u->id = 123;
+    for (i = 0; (i < 500000); i = (i + 1)) {
+                    GNode* nodeA = (GNode*)((char*)fgc_alloc(0, 1, 1) + 8);
+        nodeA->id = 1;
+                    GNode* nodeB = (GNode*)((char*)fgc_alloc(0, 1, 1) + 8);
+        nodeB->id = 2;
+                nodeA->link = nodeB;
+                nodeB->link = nodeA;
     }
     return 0;
 }
